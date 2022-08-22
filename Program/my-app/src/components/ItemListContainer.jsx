8cda35943a0f48a52604eb70ebe838c3b3ba1cc0
Ale <alonsoalex5@gmail.com>
2022-08-21 23:48:25 -0300

    commit-up

---
+++ Program/my-app/src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = () => {
+  return (
+    <div>~Home (ItemListContainer) </div>
+  )
+}
+
+export default ItemListContainer
---
+++ Program/my-app/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar: link1 / link2 / link3 / link4</div>
+  )
+}
+
+export default Navbar
